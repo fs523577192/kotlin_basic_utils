f271998bcdbfc58d26f04f0441b1b0212234cad1
Firas <448445223@qq.com>
2019-06-25 21:57:23 +0800

    Add getConcurrentHashMap

---
+++ src/commonMain/kotlin/org/firas/util/concurrent/ConcurrentHashMap.kt
@@ -0,0 +1,19 @@
+package org.firas.util.concurrent
+
+import kotlin.reflect.KClass
+
+/**
+ *
+ * @author Wu Yuping
+ */
+expect fun <K: Any, V: Any> getConcurrentHashMap(
+        keyClass: KClass<K>, valueClass: KClass<V>): MutableMap<K, V>
+
+expect fun <K: Any, V: Any> getConcurrentHashMap(
+        keyClass: KClass<K>, valueClass: KClass<V>, initialCapacity: Int): MutableMap<K, V>
+
+expect fun <K: Any, V: Any> getConcurrentHashMap(keyClass: KClass<K>, valueClass: KClass<V>,
+        initialCapacity: Int, loadFactor: Float): MutableMap<K, V>
+
+expect fun <K: Any, V: Any> getConcurrentHashMap(keyClass: KClass<K>, valueClass: KClass<V>,
+        initialCapacity: Int, loadFactor: Float, concurrencyLevel: Int): MutableMap<K, V>
---
+++ src/jsMain/kotlin/org/firas/util/concurrent/ConcurrentHashMap.kt
@@ -0,0 +1,31 @@
+package org.firas.util.concurrent
+
+import kotlin.reflect.KClass
+
+/**
+ *
+ * @author Wu Yuping
+ */
+@JsName("getConcurrentHashMap")
+actual fun <K: Any, V: Any> getConcurrentHashMap(
+        keyClass: KClass<K>, valueClass: KClass<V>): MutableMap<K, V> {
+    return HashMap()
+}
+
+@JsName("getConcurrentHashMapWithInitialCapacity")
+actual fun <K: Any, V: Any> getConcurrentHashMap(
+        keyClass: KClass<K>, valueClass: KClass<V>, initialCapacity: Int): MutableMap<K, V> {
+    return HashMap(initialCapacity)
+}
+
+@JsName("getConcurrentHashMapWithInitialCapacityAndLoadFactor")
+actual fun <K: Any, V: Any> getConcurrentHashMap(keyClass: KClass<K>, valueClass: KClass<V>,
+        initialCapacity: Int, loadFactor: Float): MutableMap<K, V> {
+    return HashMap(initialCapacity, loadFactor)
+}
+
+@JsName("getConcurrentHashMapWithInitialCapacityLoadFactorAndConcurrencyLevel")
+actual fun <K: Any, V: Any> getConcurrentHashMap(keyClass: KClass<K>, valueClass: KClass<V>,
+        initialCapacity: Int, loadFactor: Float, concurrencyLevel: Int): MutableMap<K, V> {
+    return HashMap(initialCapacity, loadFactor)
+}
